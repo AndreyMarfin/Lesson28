de322984ef144d29d4f7f0857e84f7a496020840
Андрей Марфин <andreymarfinpost@gmail.com>
2022-04-06 13:02:04 +0300

    Added printAllByCriteria in PedometerManager

---
+++ src/main/java/PedometerManager.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class PedometerManager implements Comparable<PedometerManager> {
+    private Map<Integer, Integer> data = new HashMap<>();
+    private int max = 0;
+
+    public int add(int day, int steps) {
+        if (day <= 0 || steps < 0) {
+            return max = -1;
+        } else {
+            int newValue = data.getOrDefault(day, 0) + steps;
+            data.put(day, newValue);
+            if (max < newValue) {
+                max = newValue;
+            }
+            return max + 1 - steps;
+        }
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (int steps : data.values()) {
+            sum += steps;
+        }
+        return sum;
+    }
+
+    public int dayUpperLimit(int minSteps) {
+        int response = 0;
+        for (int steps : data.values()) {
+            if (steps > minSteps) {
+                response++;
+            }
+        }
+        return response;
+    }
+
+    public void printAllByCriteria(Predicate<Integer> predicate) {
+        for (int day : data.keySet()) {
+            if (predicate.test(data.get(day))) {
+                System.out.println("День " + day + " шагов " + data.get(day));
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(PedometerManager pedometerManager) {
+        return sum() - pedometerManager.sum();
+    }
+}
